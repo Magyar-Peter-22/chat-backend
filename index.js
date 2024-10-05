ff03ddf4839bff9ee8b2bfce343423ea056921e6
Magyar-Peter-22 <81881238+Magyar-Peter-22@users.noreply.github.com>
2024-10-05 18:04:01 +0200

    more logs

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ ioRoutes(io);
 //server client
 app.use('/', (req, res) => {
   try {
-    console.log("serving client");
+    console.log(`serving client for "${req.url}"`);
     proxy.web(req, res, { target: process.env.CLIENT_URL })
   }
   catch (err) {
